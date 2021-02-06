b2ad807d35d1ace9bff7640bb27f7cfa29554550
luisangelquinones <luisangelq3673@gmail.com>
2021-02-06 00:05:19 -0600

    create connection to database and adding some tables with strappi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '81cbbe474c8d52388657ce4380e41c03'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c72282c8-c2fd-487b-a9df-54b2491cc066'
+};
